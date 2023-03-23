2edaed0483dd2f6ec46643245e6156e70eab134c
darshit1425 <darshitmendapara1425@gmail.com>
2023-03-23 11:20:23 +0530

    Initial commit

---
+++ lib/screen/homescreen/provider/home_provider.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+
+class Home_provider extends ChangeNotifier {
+  List Images = [
+    "assets/images/ss1.png",
+    "assets/images/ss2.png",
+    "assets/images/ss3.png",
+    "assets/images/ss4.png",
+    "assets/images/ss6.png",
+    "assets/images/ss7.png",
+    "assets/images/ss8.png",
+    "assets/images/ss9.jpg",
+    "assets/images/ss1.png",
+    "assets/images/ss2.png",
+    "assets/images/ss8.png",
+    "assets/images/ss9.jpg",
+    "assets/images/ss3.png",
+    "assets/images/ss6.png",
+    "assets/images/ss7.png",
+    "assets/images/ss4.png",
+    "assets/images/ss1.png",
+    "assets/images/ss2.png",
+    "assets/images/ss3.png",
+    "assets/images/ss4.png",
+    "assets/images/ss6.png",
+    "assets/images/ss7.png",
+    "assets/images/ss8.png",
+    "assets/images/ss9.jpg",
+    "assets/images/ss1.png",
+    "assets/images/ss2.png",
+    "assets/images/ss8.png",
+    "assets/images/ss9.jpg",
+    "assets/images/ss3.png",
+    "assets/images/ss6.png",
+    "assets/images/ss7.png",
+    "assets/images/ss4.png",
+  ];
+}
